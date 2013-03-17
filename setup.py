517dc84bf088edf1b37c131d9f3ad9d2d4eefef8
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2013-03-17 01:48:48 +0100

    add github link

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='Python compiler for the browser',
     author='Amirouche Boubekki',
     author_email='amirouche.boubekki@gmail.com',
-    url='',
+    url='https://github.com/amirouche/PythonScript',
     zip_safe=False,
     packages=['pythonscript'],
     entry_points="""
