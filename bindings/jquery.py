15cb2e3c754b895b5dbf2c59b848b7c357e5b1d9
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2013-03-22 08:47:59 +0100

    update jquery bindings

--- bindings/jquery.py
+++ bindings/jquery.py
@@ -129,7 +129,7 @@ class J:
         return o
 
     def detach(self, selector):
-        j = self.j
+a        j = self.j
         o = JS('j.detach(selector)')
         return o
 
@@ -156,4 +156,140 @@ class J:
     def error(self, handler):
         j = self.j
         o = JS('j.error(handler)')
+        return o
+
+    def fade_in(self, duration, complete):
+        j = self.j
+        o = JS('j.fadeIn(duration, complete)')
+        return o
+
+
+    def fade_out(self, duration, complete):
+        j = self.j
+        o = JS('j.fadeOut(handler)')
+        return o
+
+    def fadeTo(self, duration, opacity, complete):
+        j = self.j
+        o = JS('j.fade_to(handler)')
+        return o
+
+    def fade_toggle(self, duration, easing, complete):
+        j = self.j
+        o = JS('j.fade_toggle(duration, easing, complete)')
+        return o
+
+    def filter(self, selector):
+        j = self.j
+        o = JS('j.filter(selector)')
+        return o
+
+    def finish(self, queue):
+        j = self.j
+        o = JS('j.finish(queue)')
+        return o
+
+    def first(self, ):
+        j = self.j
+        o = JS('j.first()')
+        return o
+
+    def focus(self, handler):
+        j = self.j
+        o = JS('j.focus(handler)')
+        return o
+
+    def focus_in(self, handler):
+        j = self.j
+        o = JS('j.focus_in(handler)')
+        return o
+
+    def focus_out(self, handler):
+        j = self.j
+        o = JS('j.filter(selector)')
+        return o
+
+    def get(self, index):
+        j = self.j
+        o = JS('j.get(index)')
+        return o
+
+    def has_class(self, name):
+        j = self.j
+        o = JS('j.has_class(selector)')
+        return o
+
+    def height(self, value):
+        j = self.j
+        o = JS('j.height(value)')
+        return o
+
+    def hide(self, duration, complete):
+        j = self.j
+        o = JS('j.hide(duration, complete)')
+        return o
+
+    def hover(self, handler):
+        j = self.j
+        o = JS('j.hover(handler)')
+        return o
+
+    def html(self, value):
+        j = self.j
+        o = JS('j.html(value)')
+        return o
+
+    def index(self, selector):
+        j = self.j
+        o = JS('j.index(selector)')
+        return o
+
+    def inner_height(self):
+        j = self.j
+        o = JS('j.innerHeight()')
+        return o
+
+    def inner_width(self):
+        j = self.j
+        o = JS('j.innerWidth()')
+        return o
+
+    def insert_after(self, target):
+        j = self.j
+        o = JS('j.insertAfter(target)')
+        return o
+
+    def insert_before(self, target):
+        j = self.j
+        o = JS('j.insertBefore(selector)')
+        return o
+
+    def is_(self, name):
+        j = self.j
+        o = JS('j.is(selector)')
+        return o
+
+    def ajax(self, url, options):
+        j = self.j
+        o = JS('j.ajax(url, options)')
+        return o
+
+    def has_class(self, name):
+        j = self.j
+        o = JS('j.filter(selector)')
+        return o
+
+    def has_class(self, name):
+        j = self.j
+        o = JS('j.filter(selector)')
+        return o
+
+    def has_class(self, name):
+        j = self.j
+        o = JS('j.filter(selector)')
+        return o
+
+    def has_class(self, name):
+        j = self.j
+        o = JS('j.filter(selector)')
         return o
