9aba94db7b33cddb5e7bd0640e22f00af48a5cb8
hartsantler <goatman.py@gmail.com>
2013-09-29 07:09:31 -0700

    wrapping more Three.js classes

--- bindings/three.py
+++ bindings/three.py
@@ -2,21 +2,44 @@
 # by Brett Hartshorn - copyright 2013
 # License: "New" BSD
 
+class _ObjectBase:
+	def add(self, child):
+		ob = self._object
+		JS('ob.add(child)')
+
+class _PerspectiveCamera( _ObjectBase ):
+
+
 class _Scene:
 	def __init__(self):
 		self._scene = JS('new THREE.Scene()')
 
-class _CSS3DRenderer:
-	def __init__(self):
-		self._renderer = JS('new THREE.CSS3DRenderer()')
+	def add(self, child):
+		scene = self._scene
+		JS('scene.add(child)')
 
+
+class _Renderer:
 	def setSize(self, width, height):
 		renderer = self._renderer
 		JS('renderer.setSize( width, height )')
 
+	def setClearColor(self, red=1.0, green=1.0, blue=1.0, alpha=1.0):
+		renderer = self._renderer
+		JS('renderer.setClearColor( {r:red, g:green, b:blue}, alpha)')
+
+
+class _CSS3DRenderer( _Renderer ):
+	def __init__(self):
+		self._renderer = JS('new THREE.CSS3DRenderer()')
+
 	def getDomElement(self):
 		renderer = self._renderer
 		return JS('renderer.domElement')
+
+class _WebGLRenderer( _Renderer ):
+	def __init__(self):
+		self._renderer = JS('new THREE.WebGLRenderer()')
 
 
 class _ImageUtils:
@@ -37,6 +60,10 @@ class _Three:
 
 	def CSS3DRenderer(self):
 		return _CSS3DRenderer()
+
+	def WebGLRenderer(self):
+		return _WebGLRenderer()
+
 
 
 Three = _Three()
