rusthon/Rusthon

commit e589ed1a624d6c5ef4d08841822a815f354dec35

second argument of PythonJS functions must always be provided as a JSObject...

runtime/pythonpythonjs.py
@@ -190,7 +190,7 @@ def get_attribute(object, attribute):
                 def method():
                     var(args)
                     args =  Array.prototype.slice.call(arguments)
-                    if (JS('args[0] instanceof Array') and (JS("{}.toString.call(args[1]) === '[object Object]'") or args[1]) is None and args.length == 2):
+                    if (JS('args[0] instanceof Array') and JS("{}.toString.call(args[1]) === '[object Object]'") is None and args.length == 2):
                         pass
                     else:
                         # in the case where the method was submitted to javascript code

commit ded89170e2cf857ada24a4945f7209114f752d60

fixed regression introduced in e589ed1a624d6c5ef4d08841822a815f354dec35

pythonjs.js
@@ -250,7 +250,7 @@ if({}.toString.call(attr) === '[object Function]') {
 var method = function() {
 var args;
 args = Array.prototype.slice.call(arguments);
-if(args[0] instanceof Array && {}.toString.call(args[1]) === '[object Object]' || args[1] === undefined && args.length == 2) {
+if(args[0] instanceof Array && {}.toString.call(args[1]) === '[object Object]' && args.length == 2) {
 /*pass*/
 }
 else {
@@ -280,7 +280,14 @@ if(attr) {
 if({}.toString.call(attr) === '[object Function]') {
 var method = function() {
 var args;
-args = arguments;
+args = Array.prototype.slice.call(arguments);
+if(args[0] instanceof Array && {}.toString.call(args[1]) === '[object Object]' && args.length == 2) {
+/*pass*/
+}
+else {
+args = [args, Object()];
+}
+
 args[0].splice(0, 0, object);
 return attr.apply(undefined, args);
 }

runtime/pythonpythonjs.py
@@ -190,7 +190,7 @@ def get_attribute(object, attribute):
                 def method():
                     var(args)
                     args =  Array.prototype.slice.call(arguments)
-                    if (JS('args[0] instanceof Array') and JS("{}.toString.call(args[1]) === '[object Object]'") is None and args.length == 2):
+                    if (JS('args[0] instanceof Array') and JS("{}.toString.call(args[1]) === '[object Object]'") and args.length == 2):
                         pass
                     else:
                         # in the case where the method was submitted to javascript code
@@ -212,7 +212,7 @@ def get_attribute(object, attribute):
                     def method():
                         var(args)
                         args =  Array.prototype.slice.call(arguments)
-                        if (JS('args[0] instanceof Array') and JS("{}.toString.call(args[1]) === '[object Object]'") is None and args.length == 2):
+                        if (JS('args[0] instanceof Array') and JS("{}.toString.call(args[1]) === '[object Object]'") and args.length == 2):
                             pass
                         else:
                             # in the case where the method was submitted to javascript code

unittests/0009-in-operator.py.expected
@@ -16,4 +16,3 @@ in literal jsobject ok
 this is allowed because we are within-javascript where the rules are slightly different
 class name of instance b: B
 test complete
-  