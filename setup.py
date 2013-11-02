40a272ba9524f651be2b025c88bb249466f19c4e
Amirouche Boubekki <amirouche.boubekki@gmail.com>
2013-11-02 10:20:48 +0100

    version bump for master branch, getting ready for 0.8.2 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='PythonJS',
-    version='0.8',
+    version='0.8.2',
     description='Python translator for the browser',
     author='Amirouche Boubekki',
     author_email='amirouche.boubekki@gmail.com',
