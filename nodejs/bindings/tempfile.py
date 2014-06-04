1e800bac3fe9fc49a19217ad24d61bfbdae70f7c
hartsantler <goatman.py@gmail.com>
2014-06-03 20:43:26 -0700

    removed deprecated tests, bindings, and external code.

--- nodejs/bindings/tempfile.py
+++
@@ -1,7 +0,0 @@
-_os = require('os')
-
-with javascript:
-	tempfile = {
-		'gettempdir' : lambda : _os.tmpdir()
-	}
-
--- nodejs/tests/subprocess-helloworld.py
+++
@@ -1,6 +0,0 @@
-from nodejs.subprocess import *
-
-print 'testing subprocess.call'
-subprocess.call( 'ls', ['-lh'] )
-print 'test complete.'
-
