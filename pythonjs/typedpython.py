31953c2e3f4eaa0ddb82ccf26dca49fb83675637
hartsantler <goatman.py@gmail.com>
2014-07-21 03:44:49 -0700

    allow inline function `x.func( callback=def cb(): ...)` syntax

--- pythonjs/typedpython.py
+++ pythonjs/typedpython.py
@@ -23,6 +23,8 @@ __whitespace = [' ', '\t']
 
 def transform_source( source, strip=False ):
 	output = []
+	output_post = None
+
 	for line in source.splitlines():
 		a = []
 		for i,char in enumerate(line):
@@ -91,11 +93,20 @@ def transform_source( source, strip=False ):
 			c += ')' * c.count(' new ')
 			c = c.replace(' new ', ' new(')
 
+		## X.method.bind(X) shortcut `->`
 		if '->' in c:
 			a,b = c.split('->')
 			this_name = a.split()[-1].split('=')[-1].split(':')[-1].split(',')[-1]
 			method_name = b.split()[0].split('(')[0]
 			c = c.replace('->'+method_name, '.'+method_name+'.bind(%s)'%this_name)
+
+		## callback=def .. inline function ##
+		if '=def ' in c or '= def ' in c:
+			d = '=def '
+			c, tail = c.split(d)
+			#name = c.split(d)[-1].split('(')[0]
+			c += '=lambda __INLINE_FUNCTION__: %s )' %tail.strip().split(':')[0]
+			output_post = 'def %s'%tail
 
 		## jquery ##
 		## TODO ensure this is not inside quoted text
@@ -110,8 +121,17 @@ def transform_source( source, strip=False ):
 			c = c.replace('nonlocal ', 'global ')  ## fake nonlocal with global
 
 		output.append( c )
+		if type(output_post) is str:
+			output.append(output_post)
+			output_post = True
+		elif output_post == True:
+			if output[-1].strip()==')':
+				output.pop()
+				output_post = None
 
-	return '\n'.join(output)
+	r = '\n'.join(output)
+	print(r)
+	return r
 
 
 test = '''
@@ -127,6 +147,9 @@ if True:
 A.callback = B->method
 A.do_something( x,y,z, B->method )
 A.do_something( x,y,z, callback=B->method )
+A.do_something( x,y,z, callback=def cb(x):
+	return x+y
+)
 
 '''
 
