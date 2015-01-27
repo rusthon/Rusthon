a8f7f9071c3bf0cef0cebbc325a723fbdc239a4d
hartsantler <goatman.py@gmail.com>
2015-01-26 19:36:45 -0800

    method chain call style. crashes g++

---
+++ regtests/c++/chain.py
@@ -0,0 +1,22 @@
+'''
+return a class instance
+'''
+
+class A:
+	def __init__(self):
+		let self.val : float = 0
+	def add(self, a:float) ->self:
+		self.val += a
+		return self
+	def mult(self, a:float) ->self:
+		self.val *= a
+		return self
+	def div(self, a:float) ->self:
+		self.val /= a
+		return self
+
+
+def main():
+	a = A()
+	val = a.add(100.0).mult(3.0).div(2.0).val
+	print(val)
