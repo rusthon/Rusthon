51d8288f1f4d844709478cdff3168caeae9e6fc8
hartsantler <goatman.py@gmail.com>
2015-09-05 12:15:04 -0700

    script to create DEB and RPM packages using FPM.

---
+++ examples/std_move.md
@@ -0,0 +1,36 @@
+C++11 std::move
+--------------
+
+`std::move` is also used below, this is the fastest way to move strings around without making a copy,
+the original variable becomes invalid after moved.
+http://en.cppreference.com/w/cpp/utility/move
+
+```rusthon
+#backend:c++
+
+def foo( s:string&& ):
+	print 'string moved into foo'
+	print s
+
+def main():
+	vec = []string(
+		'hello',
+		'world'
+	)
+	print vec[0]
+	print vec[1]
+
+	a = 'bar'
+	b = 'xxx'
+	print 'testing std::move'
+	with MV as "std::move(%s)":
+		vec.append( MV(a) )
+		foo( MV(b) )
+	print 'done moving a and b'
+	## this prints nothing because std::move was used on a and b
+	print a
+	print b
+	print vec[2]  ## this prints `bar`
+
+
+```
---
+++ make-linux-packages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+## create DEB and RPM packages for Debian and Fedora.
+## requires: FPM
+## https://github.com/jordansissel/fpm
+## apt-get install ruby-dev gcc
+## gem install fpm
+
+rm *.deb
+rm *.rpm
+
+chmod +x rusthon.py
+sudo ln --symbolic --force /usr/lib/rusthon/rusthon.py /usr/bin/transpile
+
+## debian
+fpm -s dir -t deb -a all -n rusthon -v 0.9.0 /usr/bin/transpile ./rusthon.py=/usr/lib/rusthon/rusthon.py ./src=/usr/local/lib/rusthon/
+
+## check contents of deb
+dpkg -c rusthon_0.9.0_all.deb
+
+## fedora -- requires rpmbuild -- which is not available in debian?
+fpm -s dir -t rpm -a noarch -n rusthon -v 0.9.0 /usr/bin/transpile ./rusthon.py=/usr/lib/rusthon/rusthon.py ./src=/usr/local/lib/rusthon/
+
