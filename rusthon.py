af483b6a4ab7ddfed50758097a3bb44c9b8bd8d2
hartsantler <goatman.py@gmail.com>
2015-12-08 17:17:58 -0800

    added new command line option `--openshift-python` to quickly inline a client side js app into the wsgi.py default server.

--- rusthon.py
+++ rusthon.py
@@ -153,6 +153,58 @@ self.onmessage = function (evt) {
 }
 '''
 
+OPENSHIFT_PY = [
+u'''#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+#
+# IMPORTANT: Put any additional includes below this line.  If placed above this
+# line, it's possible required libraries won't be in your searchable path
+#
+from base64 import b64decode
+INDEX_HTML = b64decode("%s")
+''',  ## header
+
+u'''
+def application(environ, start_response):
+
+    ctype = 'text/plain'
+    if environ['PATH_INFO'] == '/health':
+        response_body = "1"
+    elif environ['PATH_INFO'] == '/env':
+        response_body = ['%s: %s' % (key, value)
+                    for key, value in sorted(environ.items())]
+        response_body = '\\n'.join(response_body)
+    else:
+        ctype = 'text/html'
+        response_body = INDEX_HTML
+
+    status = '200 OK'
+    response_headers = [('Content-Type', ctype), ('Content-Length', str(len(response_body)))]
+    #
+    start_response(status, response_headers)
+    return [response_body]
+
+#
+# Below for testing only
+#
+if __name__ == '__main__':
+    from wsgiref.simple_server import make_server
+    httpd = make_server('localhost', 8051, application)
+    # Wait for a single request, serve it and quit.
+    httpd.handle_request()
+''']
+
+
+
 def compile_js( script, module_path, main_name='main', directjs=False, directloops=False ):
 	'''
 	directjs = False     ## compatible with pythonjs-minimal.js
@@ -451,7 +503,7 @@ def is_restricted_bash( line ):
 		'cd', 'mkdir', 'cp', #'pwd', 'ls',
 		'npm', 'grunt', 'gyp', 'nw-gyp',
 		'apt-get', 'yum',
-		'pip', 'docker',
+		'pip', 'docker', 'rhc',
 	]
 	cmd = line.split()[0]
 	if cmd == 'sudo': cmd = line.split()[1]
@@ -1012,6 +1064,12 @@ def build( modules, module_path, datadirs=None ):
 
 			mod['code'] = html
 			output['html'].append( mod )
+			## inlines js app into openshift default style python server ##
+			if '--openshift-python' in sys.argv:
+				import base64
+				bcoded = base64.b64encode(mod['code'].encode('utf-8'))
+				wsgi = [ OPENSHIFT_PY[0]%bcoded, OPENSHIFT_PY[1] ]
+				output['datafiles']['wsgi.py'] = '\n'.join(wsgi).encode('utf-8')
 
 	if modules['verilog']:
 		source = []
